834539842174d45c4d6627944ea57345a5cef11e
Polunina Svetlana <andj.sv@gmail.com>
2025-05-18 20:19:13 +0300

    add --format option and command arguments

--- gendiff.js
+++ gendiff.js
@@ -2,14 +2,17 @@
 
 import { Command } from 'commander'
 
-const message = 'We have done it!'
-
 const program = new Command()
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>', 'path to first configuration file to compare')
+  .argument('<filepath2>', 'path to second configuration file to compare')
 
 program.parse()
 
-console.log(message)
+const [path1, path2] = program.args
+console.log(`format: ${program.opts().format ?? 'unknown'} ${path1} ${path2}`)
+
