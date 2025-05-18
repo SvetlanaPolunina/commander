96905ac561f217b6beb0a43af67d1ffd72f63879
Polunina Svetlana <andj.sv@gmail.com>
2025-05-18 20:25:59 +0300

    delete command arguments descriptions

--- gendiff.js
+++ gendiff.js
@@ -8,11 +8,11 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .option('-f, --format <type>', 'output format')
-  .argument('<filepath1>', 'path to first configuration file to compare')
-  .argument('<filepath2>', 'path to second configuration file to compare')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
 
 program.parse()
 
 const [path1, path2] = program.args
-console.log(`format: ${program.opts().format ?? 'unknown'} ${path1} ${path2}`)
-
+console.log(`format: ${program.opts().format ?? 'unknown'}`)
+console.log(`filepaths: ${path1} ${path2}`)
